5c840133005c0afccd95202f0e7d90e7fc6f430a
smdonaldson2 <89559516+smdonaldson2@users.noreply.github.com>
2023-01-23 22:44:37 -0500

    Update Class1.cs

--- FactorialService/Class1.cs
+++ FactorialService/Class1.cs
@@ -1,16 +1,18 @@
-﻿namespace FactorialService;
+namespace FactorialService;
 public class FactChecker
 {
     public static int FactorialFinder(int i)
     {
         if (i < 0)
-            return 0;
+            throw new Exception("No negative!");
         if (i <= 1)
             return 1;
         return i * FactorialFinder(i-1);
     }
+    
 
 }
+
 
 
 
