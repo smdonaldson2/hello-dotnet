7c6333a2404282d46f9b2b6e1c0eb218289e5d81
smdonaldson2 <89559516+smdonaldson2@users.noreply.github.com>
2023-01-23 22:44:58 -0500

    Update UnitTest1.cs

--- FactorialService.Tests/UnitTest1.cs
+++ FactorialService.Tests/UnitTest1.cs
@@ -17,36 +17,9 @@ public class FactTest
     [Fact]
     public void FactBasic3()
     {
-        Assert.Equal(0,FactChecker.FactorialFinder(-2));
-    }
-
-
-
-/*
-    [Fact]
-        public void Factorial_NoNegative()
-        {
-
-
-
-
-
-
-
-
-            int x = -2;
-
-            Assert.Throws<Exception>(()=>{
-                FactChecker.FactorialFinder(x);
-            });
-
-            if (x < 0)
-            {
-                throw new Exception("No negative!");
-            }
-
-        }
-        */
-        
+        Assert.Throws<Exception>(()=>{
+            FactChecker.FactorialFinder(-2);
+        });
+    }        
 }
 
